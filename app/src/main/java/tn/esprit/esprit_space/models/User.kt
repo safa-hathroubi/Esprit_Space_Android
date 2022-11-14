38a7a08c070d4a7b8efdea0be8367daf1cc82755
safa-hathroubi <safa.hathroubi@esprit.tn>
2022-11-15 00:50:49 +0100

    Revert "Initial commit"
    This reverts commit 67eaa0ef0a9adceb713349890c686fba370d0980.

--- app/src/main/java/tn/esprit/esprit_space/models/User.kt
+++
@@ -1,9 +0,0 @@
-package tn.esprit.esprit_space.models
-import com.google.gson.annotations.SerializedName
-import java.io.Serializable
-data class User (
-    @SerializedName("_id") val id : String,
-    @SerializedName("login") val login : String,
-    @SerializedName("password") val password : String,
-    @SerializedName("status") val status  : String,
-) :  Serializable
