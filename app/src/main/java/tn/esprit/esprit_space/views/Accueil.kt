d6b12d1c81c4564c043937123047705b756cdf4c
safa-hathroubi <safa.hathroubi@esprit.tn>
2022-11-18 15:31:49 +0100

    -Changed signup fieldtext to filled -Added messages fragment -Changed menu items in nav drawer

--- app/src/main/java/tn/esprit/esprit_space/views/Accueil.kt
+++ app/src/main/java/tn/esprit/esprit_space/views/Accueil.kt
@@ -7,31 +7,30 @@ import android.view.MenuItem
 import android.widget.Toast
 //import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.home_content.*
-import tn.esprit.esprit_space.LOGIN
-import tn.esprit.esprit_space.PREF_NAME
-import tn.esprit.esprit_space.R
+import tn.esprit.esprit_space.*
 
 
 class Accueil : AppCompatActivity() {
     private lateinit var mSharedPref: SharedPreferences
 
     lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accueil)
         setSupportActionBar(toolbar)
 
+        val dashboardFragmentInstance = MessagesFragment.newInstance("param","param")
 
         mSharedPref = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
         Toast.makeText(this@Accueil, "Welcome "+ mSharedPref.getString(LOGIN, "").toString(), Toast.LENGTH_SHORT).show()
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
@@ -42,13 +41,14 @@ class Accueil : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
 
+            it.isChecked = true
+
             when(it.itemId){
 
                 R.id.nav_home -> Toast.makeText(applicationContext,"Clicked Home",Toast.LENGTH_SHORT).show()
-                R.id.nav_message -> Toast.makeText(applicationContext,"Clicked Message",Toast.LENGTH_SHORT).show()
+                R.id.nav_message -> replaceFragment(dashboardFragmentInstance, it.title.toString())
                 R.id.nav_sync -> Toast.makeText(applicationContext,"Clicked Sync",Toast.LENGTH_SHORT).show()
                 R.id.nav_trash -> Toast.makeText(applicationContext,"Clicked Trash",Toast.LENGTH_SHORT).show()
-                R.id.nav_settings -> Toast.makeText(applicationContext,"Clicked Settings",Toast.LENGTH_SHORT).show()
                 R.id.nav_login -> Toast.makeText(applicationContext,"Clicked Login",Toast.LENGTH_SHORT).show()
                 R.id.nav_share -> Toast.makeText(applicationContext,"Clicked Share",Toast.LENGTH_SHORT).show()
                 R.id.nav_rate_us -> Toast.makeText(applicationContext,"Clicked Rate us",Toast.LENGTH_SHORT).show()
@@ -70,6 +70,16 @@ class Accueil : AppCompatActivity() {
         fragmentTransaction.commit()
     }*/
 
+
+    private fun replaceFragment(fragment: Fragment, title: String){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
